d6eed51d76bf84cdf916e632e93db9d6f6ad306b
LeeSangMuk <dltkdanr131@gmail.com>
2020-12-29 23:48:52 +0900

    20-12-29 뷰 페이지 수정

--- SimplePos/SimplePos/App.xaml.cs
+++ SimplePos/SimplePos/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SimplePos
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new SimplePos());
+            MainPage = new SimplePos();
         }
 
         protected override void OnStart()
